1bc15e6d2360baa8f960ce586ee2248e86a3dcf9
Patrick Owens <manbearpat@gmail.com>
2019-08-06 01:32:53 -0700

    updated import references, removed negatuve result page

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.scss';
 import Books from "./pages/Search";
 import Detail from "./pages/ReadList";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
