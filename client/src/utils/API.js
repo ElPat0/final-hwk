5e17ef69f38132e8cf52f05841dc2d4ea2142283
Patrick Owens <manbearpat@gmail.com>
2019-08-07 18:32:24 -0700

    typos

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export default {
   // Gets all books
-  getBooks: function() {
-    return axios.get('/api/books' );
+  getBook: function(query) {
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
   },
   // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
-  },
+  //getBook: function(id) {
+  //  return axios.get("/api/books/" + id);
+ // },
   // Deletes the book with the given id
   deleteBook: function(id) {
     return axios.delete("/api/books/" + id);
